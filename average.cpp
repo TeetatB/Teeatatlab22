3d33d600089dd4135aa29a6ff645e70c454e5c2c
TeetatB <teetatmix36108@gmail.com>
2024-02-26 11:28:14 +0700

    lab22_1 completed

--- average.cpp
+++ average.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+int main(int argc,char *argv[]){
+    double sum = 0;
+    double avg;
+    if(argc > 1){
+        for(int i =0;i < argc;i++){
+            sum += atof(argv[i]);
+        }
+        avg = sum/(argc-1);
+        cout << "---------------------------------\n";
+        cout << "Average of " << argc-1 << " numbers = " << avg;
+        cout << "\n---------------------------------";
+    }else{
+        cout << "Please input numbers to find average.";
+    }
+    
+}
